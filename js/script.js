3e33ae1f771a0d8a2ebb826f46bf8d3a3e2a0593
Shinrael <mircoelena89@gmail.com>
2024-03-11 15:54:29 +0100

    [add] change color according to seconds

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ createApp({
     return{
       name: 'Akira',
       image: '../image/toriyama-7_jpg_1600x900_crop_q85.jpg',
+      displayClock: '00:00:00',
+      isEven: false,
     }
+  },
+  methods:{
+    printClock(){
+      const d = new Date();
+      let hour = d.getHours() < 10? '0'+ d.getHours() : d.getHours();
+      let minute = d.getMinutes() < 10? '0'+ d.getMinutes() : d.getMinutes();
+      let second = d.getSeconds() < 10? '0'+ d.getSeconds() : d.getSeconds();
+      this.displayClock = `${hour}:${minute}:${second}`;
+      this.isEven = parseInt(second) % 2 === 0;
+    },
+    startClock(){
+      setInterval(()=> this.printClock(), 1000)
+    }
+  },
+  mounted(){
+    this.printClock();
+    this.startClock();
   }
 }).mount('#app')
